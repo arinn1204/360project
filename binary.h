d9e2bc6623cf47333fa2f6d45259cbb621d3915c
arinn1204 <aaron.rinn@me.com>
2015-04-09 21:02:49 -0700

    Completed Init and Mount Root

--- binary.h
+++ binary.h
@@ -9,18 +9,73 @@
 #include "util.h"
 
 
-
+//initialize everything
 
 int init(char *name) {
-	//initialize everything
-	//initialize ninodes = sp_inodes_count
-	//initialize nblocks = sp_blocks_count
-	//init imap = bg_inode_bmap
-	//init bmap = bg_block_bmap
+	int i;
+	char buf[BLKSIZE];
+	for (i = 0; i < NMINODES; i++) {
+		minode[i].refcount = 0;
+		minode[i].dev = 0;
+		minode[i].ino = 0;
+		minode[i].dirty = 0;
+		minode[i].mounted = 0;
+	}
+	root = 0;
+
+	for (i = 0; i < NFD; i++) {
+		P0->fd[i] = 0;
+		P1->fd[i] = 0;
+		running->fd[i] = 0;
+	}
+
+
+	P0->next = P1;
+	P1->next = P0;
+	running->next = P0;
+
+	P0->cwd = 0;
+	P1->cwd = 0;
+	running->cwd = 0;
 
 }
 
+//initialize ninodes = s_inodes_count
+//initialize nblocks = s_blocks_count
+//init imap = bg_inode_bitmap
+//init bmap = bg_block_bitmap
+//init inodeTable = bg_inode_table
+
 int mount_root(char *name) {
+	char buf[BLKSIZE];
+
+
+	fd = open(name, O_RDWR);
+	if (fd == -1) {
+		fprintf(stderr, "Opening device failed\n");
+		exit(-1);
+	}
+	getblock(fd, SUPERBLOCK, buf);
+	sp = (SUPER *)buf;
+	if (sp->s_magic != 0xEF53) {
+		fprintf(stderr, "Not EXT2! Exiting now.\n");
+		exit(-1);
+	}
+	printf("Ext2 ... Check\n");
+	ninodes = sp->s_inodes_count;
+	nblocks = sp->s_blocks_count;
+
+	getblock(fd, GDBLOCK, buf);
+	gp = (GD *)buf;
+	imap = gp->bg_inode_bitmap;
+	bmap = gp->bg_block_bitmap;
+	inodeTable = gp->bg_inode_table;
+
+	root = iget(fd, 2);
+
+	P0->cwd = iget(fd, 2);
+	P1->cwd = iget(fd, 2);
+
 
 }
 

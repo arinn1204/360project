8ea1cf80f05035b4ee802b549759a659f2a851c3
arinn1204 <aaron.rinn@me.com>
2015-04-21 11:31:47 -0700

    no need for script anymore

--- compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gcc main.c binary/binary.c binary/dealloc.c binary/ls.c binary/remove.c binary/create.c binary/link.c binary/mkdir.c binary/util.c fileio/open.c fileio/readwrite.c -g
-
